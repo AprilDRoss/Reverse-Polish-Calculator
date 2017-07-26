fb8359cd692f8d691da9d2626aa273371906b318
April Ross <aprilross@aprils-mbp.lan>
2017-07-26 16:25:29 -0400

    finalcommit

--- src/com/company/Main.java
+++ src/com/company/Main.java
@@ -10,34 +10,34 @@ public class Main {
         ReversePolishCalc rpc = new ReversePolishCalc();
 
         System.out.println("Testing the addition operation:");
-        String rpnExpression = "2.5,4.8,+";
-        double expectedResult = 7.3;
-        double actualResult = rpc.calculate(rpnExpression);
-        checkResult(rpnExpression, expectedResult, actualResult);
-        
+        String rpnExpressionAddition = "2.5,4.8,+";
+        double expectedResultAddition = 7.3;
+        double actualResultAddition = rpc.calculate(rpnExpressionAddition);
+        checkResult(rpnExpressionAddition, expectedResultAddition, actualResultAddition);
+
         System.out.println("Testing the subtraction operation:");
-        String rpnExpression = "6.0,4.8,-";
-        double expectedResult = 1.2;
-        double actualResult = rpc.calculate(rpnExpression);
-        checkResult(rpnExpression, expectedResult, actualResult);
+        String rpnExpressionSubtraction = "6.0,4.8,-";
+        double expectedResultSubtraction = 1.2;
+        double actualResultSubtraction = rpc.calculate(rpnExpressionSubtraction);
+        checkResult(rpnExpressionSubtraction, expectedResultSubtraction, actualResultSubtraction);
 
         System.out.println("Testing the multiplication operation:");
-        String rpnExpression = "0.5,3.0,*";
-        double expectedResult = 1.5;
-        double actualResult = rpc.calculate(rpnExpression);
-        checkResult(rpnExpression, expectedResult, actualResult);
+        String rpnExpressionMultiplication = "0.5,3.0,*";
+        double expectedResultMultiplication = 1.5;
+        double actualResultMultiplication = rpc.calculate(rpnExpressionMultiplication);
+        checkResult(rpnExpressionMultiplication, expectedResultMultiplication, actualResultMultiplication);
 
          System.out.println("Testing the division operation:");
-        String rpnExpression = "81.0, 9.0,/";
-        double expectedResult = 9.0;
-        double actualResult = rpc.calculate(rpnExpression);
-        checkResult(rpnExpression, expectedResult, actualResult);
+        String rpnExpressionDivision = "81.0, 9.0,/";
+        double expectedResultDivision = 9.0;
+        double actualResultDivision = rpc.calculate(rpnExpressionDivision);
+        checkResult(rpnExpressionDivision, expectedResultDivision, actualResultDivision);
 
         // Uncomment this test which has many operators
-        String rpnExpression = "5,1,2,+,4,*,+,3,-";
-        double expectedResult = 14.0;
-        double actualResult = rpc.calculate(rpnExpression);
-        checkResult(rpnExpression, expectedResult, actualResult);
+        String rpnExpressionFinal = "5,1,2,+,4,*,+,3,-";
+        double expectedResultFinal = 14.0;
+        double actualResultFinal = rpc.calculate(rpnExpressionFinal);
+        checkResult(rpnExpressionFinal, expectedResultFinal, actualResultFinal);
     }
 
     private static void checkResult(String expression, double expected, double actual) {
